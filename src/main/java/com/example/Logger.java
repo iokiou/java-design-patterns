92e30de7a6feef8d234594d2f358ec91b537d0c1
iokiou <kioujohnny@yahoo.gr>
2024-05-12 13:20:28 +0200

    Implementation of Singleton Pattern

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -2,6 +2,16 @@ package com.example;
 
 public class Logger {
 
+    private static Logger INSTANCE;
+
+    private Logger() {}
+
+    public synchronized static Logger getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Logger();
+        }
+        return INSTANCE;
+    }
 
     void log(String logMessage) {
         System.out.println(logMessage);
